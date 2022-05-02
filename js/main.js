639192d97ed2b3153f9bcdfcdee26026e62ee5f5
Mario <100005960.joan23@fje.edu>
2022-05-02 16:41:22 +0200

    Mario

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-function parrafos() {
-    var infonum = document.getElementById('infonum')
-    infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
-    var infonum2 = document.getElementById('infonum2')
-    if (numero << 1) {
-        var infonum2 = 'Here is your decimal'
+function MarioBin2Dec() {
+    var infobien = document.getElementById('infobien')
+    var infoerror = document.getElementById('infoerror')
+    var resultadobinario = document.getElementById('resultadobinario').value
+    var result = document.getElementById('result')
+    result.innerHTML = parseInt(resultadobinario, 2);
+    var numero = resultadobinario.charAt(resultadobinario.length - 1)
+    
+    if (resultadobinario == '') {
+        result.innerHTML = ''
+    }
+    if (numero == 0 || numero == 1) {
+        infobien.innerHTML = 'Here is your decimal!'
+        infoerror.innerHTML = ''
+    } else {
+        result.innerHTML = '🤷🏻 Waiting for a valid binary number...'
+    }
+    if (numero >> 1 || numero >> 10000) {
+        infobien.innerHTML = ''
+        infoerror.innerHTML = 'You entered a non-binary digit (please enter only 0 or 1)'
+    }
+    if (numero == '') {
+        infobien.innerHTML = ''
+        infoerror.innerHTML = ''
     }
 }
